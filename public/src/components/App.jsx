4510219a24969d571d91864be8d843537f92e59b
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-11-27 14:35:21 -0600

    further setup

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,3 +1,13 @@
 //
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    state = {};
+  }
+  render() {
+    return "x";
+  }
+}
+
 export default App;
---
+++ server/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const port = 7777;
+const app = express();
+
+app.listen(port, console.log(`Listening on port ${port}...`));
