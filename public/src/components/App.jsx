895aa5c5b04aab708d0c1f2b02592df4d81feba8
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-02 09:45:06 -0600

    further file setup

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+import ProductMaker from "./ProductMaker.jsx";
+import Stars from "./Stars.jsx";
+import Ratings from "./Ratings.jsx";
+import QuestionsAnswered from "./QuestionsAnswered.jsx";
 const axios = require("axios");
+const fetch = require("node-fetch");
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProductID: null
+      currentProduct: {}
     };
+  }
+
+  componentDidMount() {
+    axios.get("/").then(console.log);
   }
 
   render() {
     return (
       <div id="productDescriptionContainerS">
-        <p>yo</p>
+        <div id="titleOfProductS">
+          <p>TITLE</p>
+        </div>
+        <div id="productMakerContainerS">
+          <ProductMaker currentProduct={this.state.currentProduct} />
+        </div>
+        <div id="starsRatingsQuestionsS">
+          <span>
+            <Stars currentProduct={this.state.currentProduct} />
+            <Ratings currentProduct={this.state.currentProduct} />
+            <QuestionsAnswered currentProduct={this.state.currentProduct} />
+          </span>
+        </div>
       </div>
     );
   }
