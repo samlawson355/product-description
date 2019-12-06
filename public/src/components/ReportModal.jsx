c3b8b15e69fd2cdd530e87a6e62a8a703a691a52
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-06 10:15:57 -0600

    modal solid progress

--- public/src/components/ReportModal.jsx
+++ public/src/components/ReportModal.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const ReportModal = props => {
   return (
     <div id="reportModalContainerS">
-      <div id="innerReportModalContainerS">ReportModal</div>
+      <div id="innerReportModalContainerS">
+        <div id="topBarOfReportModalS">X</div>
+      </div>
     </div>
   );
 };
