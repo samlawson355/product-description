5a54d58cc489366cae5052b75109e4dc8d169df8
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-07 09:16:07 -0600

    more styling

--- public/src/components/DealDropdown.jsx
+++ public/src/components/DealDropdown.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const DealDropdown = props => {
-  return <div id="outerDealDropdownBoxS"></div>;
+  return (
+    <div id="outerDealDropdownBoxS">
+      <ul>
+        <li id="buy5Save5S">Buy 5, save 5%.</li>
+      </ul>
+      <div id="dealTextContainerS">
+        Save up to 15% in Pantry. Buy any 5 Pantry items to save 5% on your
+        order. Buy any 10, save 10%. Buy 15 or more and save 15% on your entire
+        Pantry order. Discount applied at checkout. Shop all Pantry at
+        www.amazon.com/pantry. Offered by Prime Pantry. <a>Here's how </a>
+        (restrictions apply)
+      </div>
+    </div>
+  );
 };
 
 export default DealDropdown;
