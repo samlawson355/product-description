106ecc3febcfa7e22d03136bb33d23da206c6755
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-07 12:00:24 -0600

    learnMore modal almost done with styling

--- public/src/components/LearnMoreModal.jsx
+++ public/src/components/LearnMoreModal.jsx
@@ -4,21 +4,21 @@ const LearnMoreModal = props => {
   return (
     <div id="innerLearnMoreModalContainerS">
       <div id="topBarLearnMoreS">
-        <div>Here's how</div>
-        <div>X</div>
+        <div id="learnMoreHowS">Here's how</div>
+        <div id="learnMoreXS">X</div>
       </div>
       <div id="innerLearnMoreS">
-        <div>Promotion details</div>
+        <h1 id="promoDetailsLearnMoreS">Promotion Details</h1>
         <div>
           <ul>
             <li>This is a limited time offer.</li>
             <li>
-              Offer only applies to products sold by Amazon.com or Amazon
-              Digital Services LLC (look for "sold by Amazon.com" or "sold by
-              Amazon Digital Services LLC" on the product detail page). Products
-              sold by third-party sellers or other Amazon entities will not
-              qualify for this offer, even if "fulfilled by Amazon.com" or
-              "Prime Eligible".
+              Offer only applies to products sold by Gammazon.com or Gammazon
+              Digital Services LLC (look for "sold by Gammazon.com" or "sold by
+              Gammazon Digital Services LLC" on the product detail page).
+              Products sold by third-party sellers or other Gammazon entities
+              will not qualify for this offer, even if "fulfilled by
+              Gammazon.com" or "Prime Eligible".
             </li>
             <li>Offer does not apply to digital content.</li>
             <li>Offer good while supplies last.</li>
@@ -40,7 +40,7 @@ const LearnMoreModal = props => {
               your order.
             </li>
             <li>
-              Amazon reserves the right to modify or cancel the offer at any
+              Gammazon reserves the right to modify or cancel the offer at any
               time.
             </li>
             <li>Offer is non-transferable and may not be resold.</li>
@@ -57,9 +57,9 @@ const LearnMoreModal = props => {
               If you violate any of these terms, the offer will be invalid.
             </li>
             <li>
-              Unless an Amazon Gift Card is the stated benefit of the promotion,
-              promotional codes (including those placed directly in accounts)
-              may not be redeemed for Amazon Gift Cards.
+              Unless an Gammazon Gift Card is the stated benefit of the
+              promotion, promotional codes (including those placed directly in
+              accounts) may not be redeemed for Gammazon Gift Cards.
             </li>
           </ul>
         </div>
