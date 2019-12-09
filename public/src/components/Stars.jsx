1f20b97f04bb07f84551483c2e53eaba18726269
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-09 11:41:38 -0600

    started using material, added some functionality and stars

--- public/src/components/Stars.jsx
+++ public/src/components/Stars.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import StarsDropdown from "./StarsDropdown.jsx";
+import Rating from "@material-ui/lab/Rating";
 
 class Stars extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dropDownDisplayed: false
+      dropDownDisplayed: false,
+      star1: false,
+      star2: false,
+      star3: false,
+      star4: false,
+      star5: false
     };
     this.hoverDropDown = this.hoverDropDown.bind(this);
     this.hoverGoBack = this.hoverGoBack.bind(this);
+    this.starClean = this.starClean.bind(this);
   }
 
   hoverDropDown() {
@@ -27,11 +34,88 @@ class Stars extends React.Component {
     );
   }
 
+  starClean() {
+    if (this.props.currentProduct.productRating >= 1) {
+      this.setState({
+        star1: true
+      });
+    }
+    if (this.props.currentProduct.productRating >= 2) {
+      this.setState({
+        star2: true
+      });
+    }
+    if (this.props.currentProduct.productRating >= 3) {
+      this.setState({
+        star3: true
+      });
+    }
+    if (this.props.currentProduct.productRating >= 4) {
+      this.setState({
+        star4: true
+      });
+    }
+    if (this.props.currentProduct.productRating === 5) {
+      this.setState({
+        star5: true
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.starClean();
+  }
+
   render() {
     return (
-      <div id="starsContainerS">
-        <div onMouseEnter={this.hoverDropDown}>
-          {this.props.currentProduct.productRating}/5
+      <div>
+        <div id="starsContainerS" onMouseEnter={this.hoverDropDown}>
+          <Rating
+            id="testRating1S"
+            name="numOfStarsS"
+            value={this.state.star1 ? 1 : 0}
+            readOnly={true}
+            precision={1}
+            max={1}
+            size="small"
+          />
+          <Rating
+            id="testRating2S"
+            name="numOfStarsS"
+            value={this.state.star2 ? 1 : 0}
+            readOnly={true}
+            precision={1}
+            max={1}
+            size="small"
+          />
+          <Rating
+            id="testRating3S"
+            name="numOfStarsS"
+            value={this.state.star3 ? 1 : 0}
+            readOnly={true}
+            precision={1}
+            max={1}
+            size="small"
+          />
+          <Rating
+            id="testRating4S"
+            name="numOfStarsS"
+            value={this.state.star4 ? 1 : 0}
+            readOnly={true}
+            precision={1}
+            max={1}
+            size="small"
+          />
+          <Rating
+            id="testRating5S"
+            name="numOfStarsS"
+            value={this.state.star5 ? 1 : 0}
+            readOnly={true}
+            precision={1}
+            max={1}
+            size="small"
+          />
+          {/* {this.props.currentProduct.productRating}/5 */}
         </div>
         <div>
           <StarsDropdown
